bf04f590d93da7e8a25ce3d8aa4947d6cbb7917d
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2021-04-03 11:22:36 +0200

    WIP

--- Test.java
+++ Test.java
@@ -1,4 +1,38 @@
+import javax.annotation.Nullable;
 
+/**
+ * Some Javadoc <a href="lol.html">hehe</a> haha.
+ * <i>foo</i> <b>bar</b> haha.
+ * <p><another paragraph/p>
+ */
+@Deprecated
+class C1<T extends Exception> extends Object {
+  private Object f1;
+  private @Nullable Object f2;
+  private C1 f3;
+  private C1<RuntimeException> f4;
 
+  /**
+   * Bla bla.
+   *
+   * More bla bla
+   *
+   * @param p1 does something
+   */
+  void m1(T p1) {
+  }
 
-public class Test extends Object {}
+  /**
+   * Hey what's up
+   *
+   * @param p2 some parameter.
+   * @return some value.
+   */
+  T m2(@Nullable T p2) {
+    return null;
+  }
+
+  void m3() {
+    m1(m2(null));
+  }
+}
