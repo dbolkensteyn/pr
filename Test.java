d1ae4fbbdf9ae1fa316fc0a0fa064c4ac659b5eb
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2021-04-12 19:02:19 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -80,7 +80,7 @@ class C1<T extends Exception> extends Object {
     T l2;
   }
   
-  class C2<T1, T2 extends Exception, Serializable> extends Exception implements Serializable {
+  class C2<T1, T2 extends Exception & Serializable> extends Exception implements Serializable {
     T1 f1;
     T2 f2;
   }
